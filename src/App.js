1b6478f7bf4aba78880157cc627919e8f7c27b29
Pothireddy Pravallika <m1053637@mindtree.com>
2021-03-26 16:46:32 +0530

    Completed module

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import './App.css'
 import Quiz from './Quiz'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        quiz_position: 1
-    }
-}
   render() {
     return (
       <Quiz />
--- src/Quiz.js
+++ src/Quiz.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 let quizData=require('./quiz_data.json');
 
 class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        quiz_position: 1
+    }
+}
   render(){
     return(
       <div className="QuizQuestion">
