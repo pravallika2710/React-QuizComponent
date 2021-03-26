4ace80cc760f6e3cdfa2e280de4aa2e27310e47f
Pothireddy Pravallika <m1053637@mindtree.com>
2021-03-26 16:42:46 +0530

    Completed module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
+import Quiz from './Quiz'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        quiz_position: 1
+    }
+}
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
---
+++ src/Quiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+let quizData=require('./quiz_data.json');
+
+class Quiz extends Component {
+  render(){
+    return(
+      <div className="QuizQuestion">
+        {quizData.quiz_questions[0].instruction_text}
+      </div>
+    )
+  }
+}
+export default Quiz
